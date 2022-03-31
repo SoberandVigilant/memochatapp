01739bae37b14585c72e8ed97c0ae540a0ae281f
Edikan A <73974392+SoberandVigilant@users.noreply.github.com>
2022-03-31 19:50:55 -0400

    login form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { ChatEngine } from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
+const projectID = 'bb7480a5-309a-4bd8-9f20-2b5f78b19f99';
+
 const App = () => {
+      if (!localStorage.getItem('username')) return <LoginForm />
+
     return (
         <ChatEngine
         height="100vh"
-        projectID=" bb7480a5-309a-4bd8-9f20-2b5f78b19f99"
+        projectID=" "
         userName="TheSoberOne"
         userSecret="Redeemed33" 
         renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
